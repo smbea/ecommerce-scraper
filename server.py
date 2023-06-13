f105c841c67ca8d9b3a549dc2dadc8f01396ef39
Beatriz Mendes <bea.smendes98@gmail.com>
2023-06-13 09:57:18 +0200

    feat: handle errors

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler
 from scrapper import scrapeUrl
 from urllib.parse import urlparse, parse_qs
 import json
+
 class Server(BaseHTTPRequestHandler):
   def do_HEAD(self):
     return
@@ -12,29 +13,35 @@ class Server(BaseHTTPRequestHandler):
   def do_POST(self):
     return
     
-  def handle_http(self, status, content_type):
-    self.send_response(status)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    parsed = urlparse(self.path)
-
+  def handle_http(self):
     print("request path: " + self.path)
 
+    parsed = urlparse(self.path)
+    url = parse_qs(parsed.query)['url'][0]
 
-    try:
-      url = parse_qs(parsed.query)['url'][0]
-    except:
-      return "Can not read url"
+    if not isValidUrl(url):
+      return 400, json.dumps({"message": "Invalid url"})
     
     try:
       info = scrapeUrl(url)
-      return json.dumps(info)
+      if info == "Not supported yet":
+        return 404, json.dumps({"message": "Website not supported yet"})
+      return 200, json.dumps(info)
     except:
-      return "Error"
+      return 500, json.dumps({"message": "Unknown error occured"})
 
     
   def respond(self):
-    content = self.handle_http(200, 'application/json')
+    status, content = self.handle_http()
+    self.send_response(status)
+    self.send_header('Content-type', 'application/json')
+    self.end_headers()
     self.wfile.write(bytes(content, "UTF-8"))
 
     
+def isValidUrl(url):
+  try:
+    result = urlparse(url)
+    return all([result.scheme, result.netloc])
+  except:
+    return False
